0285f163223cee2f7d0d0c36c2016eb51d59d170
AexP999 <76060734+AexP999@users.noreply.github.com>
2021-03-30 14:54:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const someFn = (text) => `${text} 321`
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
