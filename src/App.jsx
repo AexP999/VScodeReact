58c017a6250107b1dec88ae4c2606c43df319db9
AlexP999 <mikol999@gmail.com>
2021-04-01 08:38:57 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,181 +1,189 @@
-// import React, { Component } from 'react';
-// import './App.css'
+import React, { useEffect, useState } from 'react';
+import './App.css'
 
-// let interval;
-// class Comp extends Component {
-//   componentDidMount() {
-//     interval = setInterval(() => {
-//       console.log('fetching air data');
-//     }, 2000)
-//     console.log('Component Did Mount Child');
-//   }
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log('Component Did Updated child');
+const Buttons = ({ urlArr, getAnyJS }) => {
+  return (
+    <div className='butn'>
+      {urlArr.map((el, index) => {
+        return (<button key={index} onClick={() => getAnyJS(el.name)}>{el.name}</button>)
+      })}
+    </div>
+  )
+}
 
-//   }
-//   componentWillUnmount() {
-//     clearInterval(interval);
-//     console.log('Component Will Unmount child');
-//   }
-//   render() {
-//     console.log('child render');
-//     return (
-//       <h1>child</h1>
-//     );
-//   }
-// }
+const Posts = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>userId: {item.userId}</p>
+                <p>id: {item.id}</p>
+                <p>title: {item.title}</p>
+                <p>body: {item.body}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
+const Albums = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>userId: {item.userId}</p>
+                <p>id: {item.id}</p>
+                <p>title: {item.title}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
+const Photos = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>albumId: {item.albumId}</p>
+                <p>id: {item.id}</p>
+                <p>title: {item.title}</p>
+                <p>url: {item.url}</p>
+                <p>thumbnailUrl: {item.thumbnailUrl}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
+const Comments = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>postId: {item.postId}</p>
+                <p>id: {item.id}</p>
+                <p>name: {item.name}</p>
+                <p>email: {item.email}</p>
+                <p>body: {item.body}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
+const Todos = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>userId: {item.userId}</p>
+                <p>id: {item.id}</p>
+                <p>title: {item.title}</p>
+                <p>completed: {item.completed}</p>
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
+const Users = ({ fetchedData, isLoading }) => {
+  return (
+    <div>
+      { isLoading ? <h3>Loadind DATA...</h3> :
+        <ul >
+          {!!fetchedData.length && fetchedData.map((item) => {
+            return (
+              < li key={item.id}>
+                <p>id: {item.id}</p>
+                <p>name: {item.name}</p>
+                <p>username: {item.username}</p>
+                <p>email: {item.email}</p>
+                <p>phone: {item.phone}</p>
+                <p>website: {item.website}</p>
 
-
-
-// class App extends Component {
-//   state = {
-//     counter: 0
-//   }
-//   componentDidMount() {
-//     console.log('Componet Did Mount Parent');
-//   }
-
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log('Component Did Updated parent');
-//     // this.stat  });
-//   }
-
-//   componentWillUpdate() {
-//     console.log('Component Will Unmount parent');
-//   }
-
-//   incCounter = () => {
-//     this.setState({ counter: this.state.counter + 1 })
-//   }
-
-
-//   render() {
-//     console.log('parent render');
-
-
-//     return (
-//       <div className='App'>
-//         <h1 onClick={this.incCounter}>Hello {this.state.counter}</h1>
-//         {!!(this.state.counter % 2) && <Comp />}
-//       </div>
-//     );
-//   }
-// }
-
-
-// === Переделываем то же на функц. компонентах===
-
-// import React, { useState } from 'react';
-// import './App.css'
-// let interval;
-
-// const Comp = () => {
-//   React.useEffect(() => {
-//     console.log('Componet Did Mount child');
-//   }, []) // ===аналог componentDidMount с пустым массивом===
-
-//   React.useEffect(() => {
-//     interval = setInterval(() => {
-//       console.log('fetching air data');
-//       // ===Здесь можно писать логику для Didmount===
-//     }, 2000)
-//     return () => {
-//       clearInterval(interval);
-//       console.log('Componet Will Unmount child');
-//       // ===Здесь можно писать логику для WillUnmount===
-//     } // ===аналог componentWillUnmount с пустым массивом===
-//   }, [])
-
-
-//   console.log('child render')
-//   return (
-//     <h1>child</h1>
-//   )
-// }
-
-
-// const App = () => {
-//   const [counter, setCounter] = React.useState(0);
-
-//   const incCounter = () => {
-//     setCounter(counter + 1)
-//   }
-//   React.useEffect(() => {
-//     console.log('Componet Did Mount Parent')
-//   }, []) // ===аналог componentDidMount с пустым массивом===
-
-//   React.useEffect(() => {
-//     // if (counter) {     // запускается первый раз и потом после изменения counter. чтобы этого избежать условие if, чтобы первый раз не пускалось
-//     //   console.log('Componet Did Update Parent');
-//     // }
-//     console.log('Componet Did Update Parent');
-//     return () => console.log('Componet Will Unmount Parent');
-//     // === сначала выполняется return (), а потом Did Update Parent. т.е. сначала после return можно подчистить, а потом запустить выше===
-//   }, [counter]) // ===аналог componentDidUpdate с пустым массивом===
-
-
-//   console.log('parent render')
-//   return (
-//     <div className='App'>
-//       <h1 onClick={incCounter}>Hello {counter}</h1>
-//       {!!(counter % 2) && <Comp />}
-//     </div>
-
-//   )
-// }
-
-
-// export default App;
-
-// ====пример с фетчами====
-
-// import React, { useEffect, useState } from 'react';
-// import './App.css'
-
-// const url = ' https://jsonplaceholder.typicode.com/todos';
-
-// const App = () => {
-//   const [counter, setCounter] = React.useState(1);
-//   const [todos, setTodos] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   const incCounter = () => {
-//     setCounter(counter + 1)
-//   }
-
-//   const fetchTodos = async () => {
-//     setIsLoading(true)
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     // setTimeout(() => { //эмуляция работы с
-//     setTodos(data)
-//     setIsLoading(false)
-//     // }, 1000)
-//   }
-
-//   useEffect(() => {
-//     fetchTodos()
-//   }, [counter])
+              </li>
+            )
+          })}
+        </ul>
+      }
+    </div>
+  )
+}
 
 
-//   return (
-//     <div>
-//       <h1 onClick={incCounter}>Hello {counter}</h1>
-//       {!todos.length && isLoading && (<h2>Loadin DATA..</h2>)
-//       }
+const jSonUrl = 'https://jsonplaceholder.typicode.com/';
 
-//       {!!todos.length && (
-//         todos.map((el) => {
-//           return (
-//             <>
+const App = () => {
+  const urlArr = [
+    { name: 'posts' },
+    { name: 'albums' },
+    { name: 'photos' },
+    { name: 'comments' },
+    { name: 'todos' },
+    { name: 'users' },
+  ]
 
-//               <hr />
-//               <h3>{el.title} - {el.completed.toString()}</h3>
-//               <hr />
-//             </>)
-//         })
-//       )}
-//     </div>
-//   )
-// }
-// export default App;
+  const [fetchedData, setfetchedData] = useState([]);
+  const [usedElement, setusedElement] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    usedElement && fetchData();
+  }, [usedElement]);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch(`${jSonUrl}${usedElement}`);
+    const data = await response.json();
+    setfetchedData(data);
+    setIsLoading(false);
+  }
+
+  const getAnyJS = (usedName) => {
+    console.log('usedName', usedName);
+    setusedElement(usedName)
+  }
+
+  return (
+    <div className='App'>
+      <Buttons urlArr={urlArr} getAnyJS={getAnyJS} />
+
+      {usedElement === urlArr[0].name && <Posts fetchedData={fetchedData} isLoading={isLoading} />}
+      {usedElement === urlArr[1].name && <Albums fetchedData={fetchedData} isLoading={isLoading} />}
+      {usedElement === urlArr[2].name && <Photos fetchedData={fetchedData} isLoading={isLoading} />}
+      {usedElement === urlArr[3].name && <Comments fetchedData={fetchedData} isLoading={isLoading} />}
+      {usedElement === urlArr[4].name && <Todos fetchedData={fetchedData} isLoading={isLoading} />}
+      {usedElement === urlArr[5].name && <Users fetchedData={fetchedData} isLoading={isLoading} />}
+    </div>
+
+  )
+}
+
+
+export default App;
