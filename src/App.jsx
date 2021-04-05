e1fab56826ecb51120d9038eca01e0ce0f9a55bf
AlexP999 <mikol999@gmail.com>
2021-04-05 17:40:58 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css'
 
+// створти 2 інтупи і кнопку
+// перший відповідає за ендпоінт джсон плейсхолдера(перша частина енпоніту)
+// другий - за айдішнік
+
+// якщо другого ендпоінту нема - тягнемо весь список
+
+// потрібно зробити валідацію
+// на перший інпут - чи ендпоінт існуючий
+// на другий - чи це число і чи воно в рамках 1 - 100
+
+// зробити версію на функціональній компоненті
+// контрольовану і не контрольовану
+
+// якщо є час - на класовій компоненті теж таке саме написати
+
+
+const JSP_URL = 'https://jsonplaceholder.typicode.com/';
+const VALID_VALUES = [
+  'posts',
+  'comments',
+  'albums',
+  'users',
+  'todos',
+  'phitis'
+]
 
 function App() {
-  return (
-    <div>
+  const [enteredData, setEnteredData] = useState({
+    endPoint: '',
+    id: '',
+  })
+  const [updatedataJson, setupdatedataJson] = useState([]);
+  const [validateIdNumber, setValidateIdNumber] = useState('');
 
+  const handleSubmit = () => {
+    if (!VALID_VALUES.includes(enteredData.endPoint)) return;
+
+    if (!Number(enteredData.id) && enteredData.id !== '') return;
+
+    if (enteredData.endPoint === 'posts') {
+      console.log('call validateId()');
+      validateId()
+    }
+    console.log(enteredData.endPoint);
+    fetchData();
+  }
+
+  const validateId = () => {
+    console.log('enteredData.id2', enteredData.id);
+    console.log('updatedataJson.length', updatedataJson.length);
+    if (enteredData.id >= 1 && enteredData.id <= 100) {
+      return
+    } else {
+      console.log('>100');
+      setValidateIdNumber(`Please enter number from 1 to 100`);
+    }
+  }
+
+  const fetchData = async () => {
+    console.log('fetch');
+    const response = await fetch(`${JSP_URL}${enteredData.endPoint}/${enteredData.id}`);
+    const data = await response.json();
+    setupdatedataJson(data);
+    console.log('data', data);
+
+  }
+
+  const updateInput = (e) => {
+    // debugger
+    const { target: { value, name } } = e;
+
+    setEnteredData({ ...enteredData, [name]: value })
+
+  }
+  console.log('render');
+  return (
+    <div className='App'>
+      <h4>Hw</h4>
+      <input
+        type="text"
+        value={enteredData.endPoint}
+        onChange={updateInput}
+        name='endPoint'
+        placeholder='enter JSP endpoint'
+      />
+      <br />
+      <input
+        type="text"
+        value={enteredData.id}
+        onChange={updateInput}
+        name='id'
+        placeholder='enter JSP id'
+      />
+      <br />
+      <button onClick={handleSubmit}>Press</button>
+
+      {<p>{validateIdNumber}</p>}
+
+      <ul>
+        {updatedataJson.id ?
+          (<li>id: {updatedataJson.id} <br /> name: {updatedataJson.name ?? ' n/a'}<br /> title: {updatedataJson.title ?? ' n/a'}</li>) :
+          (!!updatedataJson.length && updatedataJson.map((elem, i) => {
+            return (<li key={i}>id: {elem.id} <br /> name: {elem.name ?? ' n/a'} <br /> title: {elem.title ?? ' n/a'}</li>)
+          }))}
+      </ul>
     </div>
   );
 }
