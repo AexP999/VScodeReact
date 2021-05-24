8561e372008095bb5ad6a08a28d5a99ea051cd1f
AlexP999 <mikol999@gmail.com>
2021-05-24 11:42:40 +0300

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,15 @@
-import React, { useEffect, useState, Component } from 'react'
-import './App.css'
-let url = 'https://jsonplaceholder.typicode.com/'
+import React from 'react'
 
-// потрібно створити логіку, яка задовільнить наступні вимоги
-// в нас має бути 6 кнопок, які дозволяють нам переключатись між 'табами' (posts, comments, albums, photos, todos, users)
-// дефолтно обрана таба- пости
-// по кліку на кнопку ми повинні підтягнути відповідний список і зрендерити його через .map
-// лише 1 список видимий одночасно
-// потрібно створити 6 компонент, які займатимуться рендерінгом списків (отримуватимуть пропсами список)- PostList, CommentsList...
-
-const PostsComponent = ({ props }) => {
-  console.log('props', props);
-  return (
-    <section>
-      <div>
-        {props.title}
-      </div>
-    </section>
-  )
-}
 
 function App() {
 
-  // это упрощенный код - только одна кнопка, нет мар. по нажатию кнопки должен выводиться один элемент posts. я думал, что переда. пропс в компоненту - она должна перерендиваться (так сказано в лекции). фактически вобще ничего не выводится
-  const [fetchedData, setFetchedData] = useState([]);
 
-
-  const fetchEndPoint = async () => {
-    const response = await fetch(url + 'posts/1')
-    const data = await response.json()
-    console.log(data)
-    setFetchedData(data);
-  }
 
   return (
     <>
-      <button onClick={fetchEndPoint}>Posts are here</button>
-      <PostsComponent props={fetchedData} />
+      <div>
+        Hello
+     </div>
     </>
   )
 }
