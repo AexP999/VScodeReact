d093247a188a990ff7a34552fd1c9c6311169fad
AlexP999 <mikol999@gmail.com>
2021-08-16 11:51:25 +0300

    2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from './redux'
+
 
 ReactDOM.render( 
   <React.StrictMode>
