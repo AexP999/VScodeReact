1dde9b057858edf39c7a4599a5c977b512350df4
AlexP999 <mikol999@gmail.com>
2021-08-26 16:28:17 +0300

    переделал на стрел компоненту

--- src/components/button-group/Sandbox.jsx
+++ src/components/button-group/Sandbox.jsx
@@ -1,51 +1,47 @@
 /* eslint-disable */
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import ButtonGroup from './ButtonGroup';
 import Button from '../button/Button';
 
-class Sandbox extends Component {
-  state = {
-    switchOn: true,
-  }
+const Sandbox = () => {
+  const [ switchOn, setSwitchOn ] = useState(true);
 
-  handleSwitchOn = () => {
-    this.setState({ switchOn: true });
-  }
 
-  handleSwitchOff = () => {
-    this.setState({ switchOn: false });
-  }
+  const handleSwitchOn = () => {
+    setSwitchOn(true);
+  };
 
-  render() {
-    const { switchOn } = this.state;
+  const handleSwitchOff = () => {
+    setSwitchOn(false);
+  };
 
-    return (
-      <Fragment>
+  return (
+    <Fragment>
 
-        <h2><span>1. Horizontal button group:</span></h2>
-        <ButtonGroup>
-          <Button active>First</Button>
-          <Button>Middle</Button>
-          <Button>Last</Button>
-        </ButtonGroup>
+      <h2><span>1. Horizontal button group:</span></h2>
+      <ButtonGroup>
+        <Button active>First</Button>
+        <Button>Middle</Button>
+        <Button>Last</Button>
+      </ButtonGroup>
 
-        <h2><span>2. Vertical button group:</span></h2>
-        <ButtonGroup vertical>
-          <Button>First</Button>
-          <Button active>Middle</Button>
-          <Button>Last</Button>
-        </ButtonGroup>
+      <h2><span>2. Vertical button group:</span></h2>
+      <ButtonGroup vertical>
+        <Button>First</Button>
+        <Button active>Middle</Button>
+        <Button>Last</Button>
+      </ButtonGroup>
 
-        <h2><span>3. Switcher button group:</span></h2>
-        <ButtonGroup>
-          <Button onClick={this.handleSwitchOn} active={switchOn}>ON</Button>
-          <Button onClick={this.handleSwitchOff} active={!switchOn}>OFF</Button>
-        </ButtonGroup>
+      <h2><span>3. Switcher button group:</span></h2>
+      <ButtonGroup>
+        <Button onClick={ handleSwitchOn } active={ switchOn }>ON</Button>
+        <Button onClick={ handleSwitchOff } active={ !switchOn }>OFF</Button>
+      </ButtonGroup>
 
-      </Fragment>
-    );
-  }
+    </Fragment>
+  );
 }
+  ;
 
 export default Sandbox;
-/* eslint-enable */
+
