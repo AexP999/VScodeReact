5f1b522df84400c59cd4fe9fb2db6e09a9de993a
AlexP999 <mikol999@gmail.com>
2021-11-11 21:40:58 +0200

    Codewars 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Loader } from './Components/Loader/Loader.jsx';
-import { Tables } from './Components/Tables/Tables.jsx';
-import { Header } from './Components/Header/Header';
+
 
 function App () {
-  const [ data, setData ] = useState([]);  // Стейт для полученных с фетч данных
-  const [ isLoading, setIsloading ] = useState(false); // Стейт для значка загрузки
 
-  const fetchUrl = async (props) => {  // Фэтч. Пропсы с Header
-    setIsloading(true);
-    const response = await fetch(props);
-    const data = await response.json();
-
-    setData(data);
-    setIsloading(false);
-  };
-
-  console.log('render');
   return (
     <div className='App'>
-      <Header fetchUrl={ fetchUrl } />    {/* Получение данных с API комп Header */ }
-      { isLoading ? <Loader /> :
-        (data.length !== 0 ? <Tables data={ data } /> : null)  // Основной компонент Header 
-      }
+
+
+      asd
+
+
+
     </div>
   );
 };
 export default App;
-// test
