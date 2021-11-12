76d3dfcd1e8b9a62dc2651492bf3f9db3d30797f
AlexP999 <mikol999@gmail.com>
2021-11-12 09:22:02 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,67 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 
 function App () {
+  //------------------------------------------------------
+  // Valid Parentheses
+
+  // Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.The function should return true if the string is valid, and false if it's invalid.
+
+  // "()"              => true;
+  // ")(()))"          => false;
+  // "("               => false;
+  // "(())((()())())"  => true
+
+  // function validParentheses (parens) {
+  //   let pairArr = parens.split('');
+
+  //   delPair();
+
+  //   function delPair () {
+  //     let length = pairArr.length;
+  //     console.log('start length', length);
+  //     for(let i = 0; i < pairArr.length; i++) {
+  //       console.log("pairArr[ i ] === '('", pairArr[ i ] === '(', i);
+  //       console.log('pairArr[ i + 1 ] === ")"', pairArr[ i + 1 ] === ')', i);
+
+  //       if(pairArr[ i ] === '(' && pairArr[ i + 1 ] === ')') {
+  //         console.log('here in if');
+  //         pairArr.splice(i, 2);
+  //         console.log('pairArr.length', pairArr.length, 'length', length);
+  //       }
+  //     }
+  //     if(length === pairArr.length) {
+  //       return console.log('end');
+  //     }
+  //     delPair();
+  //     return pairArr.length;
+  //   }
+  //   if(pairArr.length === 0) { return true; }
+  //   return false;
+  // }
+  // console.log(validParentheses('(((())))'));
+  //------------------------------------------------------
+
+  //------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+  //------------------------------------------------------
+
 
   return (
     <div className='App'>
 
 
-      asd
+      CODEWARS
 
 
 
